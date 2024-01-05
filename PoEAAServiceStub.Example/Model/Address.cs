00c97b07517eaa6edadbc163082080657f7d636c
Kaio Silveira <silveira.kaio@icloud.com>
2024-01-05 11:55:25 +0000

    feat: add `Address`

---
+++ PoEAAServiceStub.Example/Model/Address.cs
@@ -0,0 +1,8 @@
+namespace PoEAAServiceStub.Example.Model;
+
+public class Address(string street, string city, string state)
+{
+	public readonly string Street = street;
+	public readonly string City = city;
+	public readonly string State = state;
+}
